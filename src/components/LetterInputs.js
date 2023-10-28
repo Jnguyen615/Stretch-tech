e886c60f89fe16fa135c4cb13e12840b4d328b21
Nicole Rue <nicolerue92@gmail.com>
2023-10-28 09:03:46 -0700

    add comments to new lines of code

--- src/components/LetterInputs.js
+++ src/components/LetterInputs.js
@@ -5,30 +5,43 @@ import { increment } from "../reducers/Increment";
 import { useSelector } from "react-redux";
 
 function LetterInputs({ wordLength }) {
+  // Initializing an array to track the input states for each letter
+  // The 'Array(wordLength).fill("")' expression creates an array of 'wordLength' elements, and fills it with empty strings, representing the initial state of each input field. Saw this in a tutorial.
+
   const [letterStates, setLetterStates] = useState(Array(wordLength).fill(""));
 
+  // Function to update the state of a letter input field at a given index
   const updateLetterState = (index, value) => {
+    // Create a copy of the current letterStates array
     const updatedStates = [...letterStates];
+    // Update the value of the input at the specified index
     updatedStates[index] = value;
+    // Set the state with the updated array, effectively updating the input field
     setLetterStates(updatedStates);
   };
 
+  // Function to clear all input fields
   const clearInputFields = () => {
+    // Creating an array of the same length as 'wordLength' with empty values
     const clearedStates = Array(wordLength).fill("");
+    // Setting the state with the cleared input values, resetting all input fields
     setLetterStates(clearedStates);
   };
 
+  // Getting access to Redux dispatch to send actions and the counter value from the store
   const dispatch = useDispatch();
   const counterValue = useSelector((state) => state.increment.value);
 
   return (
     <>
       <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+        {/* Create an array of input fields based on wordLength. I saw this in a tutorial once, I can't say I know how it's working. sorry!  */}
         {Array.from({ length: wordLength }, (_, i) => (
           <input
             maxLength={1}
             key={i}
             value={letterStates[i]}
+            // confession: ChatGBT helped me with this below line, I couldn't figure it out on my own!
             onChange={(event) => updateLetterState(i, event.target.value)}
           />
         ))}
--- src/reducers/Increment.js
+++ src/reducers/Increment.js
@@ -1,12 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// Defining an initial value for the state
 const initialStateValue = 0;
 
+// Creating a Redux slice using createSlice
 export const wordSlice = createSlice({
+  // Naming the slice - I think it's representing a part of the application state
   name: "increment",
+  // Initial state object with a 'value' property
   initialState: { value: initialStateValue },
   reducers: {
+    // This reducer is named 'increment'
     increment: (state) => {
+      // It's going to modify the state by increasing the 'value' property by 1
       state.value += 1;
     },
   },
