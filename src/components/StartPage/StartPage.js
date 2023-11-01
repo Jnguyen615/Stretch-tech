03381e550ff9f6a675c0c41f8081d4bcce89c17f
Jen Nguyen <jenniferannnguyen@live.com>
2023-11-01 10:10:58 -0600

    Add styling to the start page

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -1,15 +1,30 @@
 import { useEffect, useLayoutEffect } from "react";
 import { Link } from "react-router-dom";
+import "./StartPage.css";
+import yellowFish from "../../images/yellow-fish.png";
+import blueFish from "../../images/blue-fish.png";
+import happySeal from "../../images/happy.png";
 
 function StartPage() {
   return (
-    <div className="start-page-container">
-      <h1>start</h1>
-      <Link to="/game">
-        <button>Start!</button>
-      </Link>
-      <h2>Help Speckle Get His Snacks!</h2>
-    </div>
+    <main>
+        <div className="start-page-container">
+          <h1>Speckle!</h1>
+          <img className="happy-seal" src={happySeal}></img>
+          <h2>Help Speckle Get His Snacks!</h2>
+          
+          <Link to="/game">
+            <button className="start-btn">Start!</button>
+          </Link>
+          <div className='start-page-fish-container'>
+          <img className="yellow-fish" src={yellowFish}></img>
+          <img className="blue-fish" src={blueFish}></img>
+          <img className="yellow-fish" src={yellowFish}></img>
+          <img className="blue-fish" src={blueFish}></img>
+          <img className="yellow-fish" src={yellowFish}></img>
+          </div>
+        </div>  
+    </main>
   );
 }
 
